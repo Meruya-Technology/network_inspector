b56fa4196861b9e03e93ae72ee9a524b301411b4
Dwi Kurnianto Mulyadien <dwikurnianto.mulyadien@gmail.com>
2023-02-01 10:49:44 +0700

    Fix github action 3 (#54)
    * Update pub publish
    
    * Update pub login

--- pub_login.sh
+++ pub_login.sh
@@ -24,8 +24,7 @@ if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
 fi
 
 # Create credentials.json file.
-
-cat <<EOF > /opt/hostedtoolcache/flutter/.pub-cache/credentials.json
+cat <<EOF > $XDG_CONFIG_HOME/dart/credentials.json
 {
   "accessToken":"${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
   "refreshToken":"${PUB_DEV_PUBLISH_REFRESH_TOKEN}",
