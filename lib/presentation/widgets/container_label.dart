72c80b2ccc49337cc44a6781f492849ffdc5ed1b
Dwi Kurnianto Mulyadien <dwikurnianto.mulyadien@gmail.com>
2023-02-01 10:12:55 +0700

    Resolve warning

--- lib/presentation/widgets/container_label.dart
+++ lib/presentation/widgets/container_label.dart
@@ -18,13 +18,13 @@ class ContainerLabel extends StatelessWidget {
         horizontal: 8,
         vertical: 4,
       ),
-      child: Text(
-        text ?? 'N/A',
-        style: Theme.of(context).textTheme.caption?.copyWith(color: textColor),
-      ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
         color: color,
+      ),
+      child: Text(
+        text ?? 'N/A',
+        style: Theme.of(context).textTheme.caption?.copyWith(color: textColor),
       ),
     );
   }
