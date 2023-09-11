fbb61dd457c3fda3f8d2719740f84d3c8a62e7ac
Dwi Kurnianto Mulyadien <dwikurnianto.mulyadien@gmail.com>
2023-09-11 07:56:50 +0700

    Update deps version

--- lib/common/utils/use_case.dart
+++ lib/common/utils/use_case.dart
@@ -4,6 +4,7 @@ abstract class UseCase<T, P> {
   Future<T> execute(P param) async {
     /// Validate usecase
     /// Execute
+    /// ignore: body_might_complete_normally_catch_error
     return await build(param).catchError((error) {
       handleError(error);
     });
